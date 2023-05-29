e67e527f1c51963277dbc3971a899e1da09ffd47
Diego Ueda <dyhueda@gmail.com>
2023-05-29 18:58:09 +0900

    finalizing the creation of the form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,7 +1,10 @@
+import Dropdown from '../Dropdown'
 import TextField from '../TextField'
+import Button from '../Button'
 import './Form.css'
 
 function Form() {
+    const items = ['Programing','Front-End','Data Science', 'DevOps', 'UX e Design', 'Mobile']
     return (
         <section className='form'>
             <form>
@@ -9,6 +12,8 @@ function Form() {
                 <TextField label='Name' placeholder='Enter your name' />
                 <TextField label='Role' placeholder='Enter your role' />
                 <TextField label='Image' placeholder='Enter the adress of your image' />
+                <Dropdown items={items} label='Team'/>
+                <Button name='Create Profile' />
             </form>
         </section>
 
